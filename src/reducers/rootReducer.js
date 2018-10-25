53c588bd9df77d5e18d29a5dc0eb4fae2abb7b24
hfgl-yilin <biel.cslayman@gmail.com>
2018-10-25 15:59:41 +0800

    update

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,7 +2,16 @@ import { combineReducers } from 'redux';
 import { productsReducer } from "./productsReducer";
 import { cartReducer } from "./cartReducer";
 
-export const rootReducer = combineReducers({
+const rootReducer = combineReducers({
     products: productsReducer,
     cart: cartReducer
 });
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    stateReconciler: autoMergeLevel2, // see "Merge Process" section for details.
+    blacklist: ['products'] // initial states from productsReducer can be updated
+   };
+
+   export const pReducer = persistReducer(persistConfig, rootReducer);
